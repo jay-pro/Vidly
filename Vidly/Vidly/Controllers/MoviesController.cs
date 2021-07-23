1cae3700e91e25d2e10341e6ed9d664ebf70574a
河儿 <60065901+jay-pro@users.noreply.github.com>
2021-07-24 00:10:08 +0700

    update MoviesControllers.cs

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public ActionResult Index()
+        // GET: Movies/Random
+        public ActionResult Random()
         {
+            var movie = new Movie() { Name = "Shrek!" };
+
             return View();
         }
     }
